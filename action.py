c546cad3a724fbdb69dd32a58584aef5b1c507fc
Kouichi1229 <jpr64914@gmail.com>
2022-10-17 12:04:23 +0800

    Update action.py

--- action.py
+++ action.py
@@ -11,7 +11,7 @@ encoding='utf-8-sig'
 @st.cache
 def convert_df(df):
 # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode(encoding).decode("latin1")
+    return df.to_csv().encode(encoding)
 
 
 def to_excel(df):
